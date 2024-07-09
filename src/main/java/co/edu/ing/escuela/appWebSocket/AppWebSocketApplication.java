6ec6b1775c4d8383ce72bea657e18cc8dc51f98d
Jefferson Mesa <mesajefer2@gmail.com>
2024-07-09 10:25:36 -0500

    add all

---
+++ src/main/java/co/edu/ing/escuela/appWebSocket/AppWebSocketApplication.java
@@ -0,0 +1,14 @@
+package co.edu.ing.escuela.appWebSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebSocketApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AppWebSocketApplication.class, args);
+	}
+
+}
